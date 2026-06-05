77e1702573ec06b7270f0217e66ecc8650092e06
Nicolas Morey <nmorey@suse.com>
2026-06-05 11:11:39 +0200

    Common#parent_module: split accessor from parent compute function
    Also support a class and not an object of that class as an arg
    
    Signed-off-by: Nicolas Morey <nmorey@suse.com>

--- lib/cli_class_tool/common.rb
+++ lib/cli_class_tool/common.rb
@@ -93,20 +93,29 @@ module CLIClassTool
         include CLIClassTool::Logger
 
         private
+        # Compute the parent module of an object or a class
+        #
+        # @param obj [Object,Class] Object or class to get the Module from
+        # @raise [StandardError] If command failed
+       def obj_to_parent_mod(obj)
+            theClass = obj.is_a?(Class) ? obj : obj.class
+            if theClass.name.nil?
+                return Object
+            else
+                parts = theClass.name.split('::')
+                if parts.size <= 1
+                    raise "CLIClassTool action classes must be defined within a named module/class namespace"
+                end
+                return Object.const_get(parts[0...-1].join('::'))
+            end
+        end
 
         # Get the parent module of this class (e.g. KernelWork or XXX)
         def parent_module
-            @parent_module ||= begin
-                if self.class.name.nil?
-                    Object
-                else
-                    parts = self.class.name.split('::')
-                    if parts.size <= 1
-                        raise "CLIClassTool action classes must be defined within a named module/class namespace"
-                    end
-                    Object.const_get(parts[0...-1].join('::'))
-                end
-            end
+            return @parent_module if @parent_module != nil
+
+            @parent_module = obj_to_parent_mod(self)
+            return @parent_module
         end
 
         # Raise error if system command failed
